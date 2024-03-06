bca3be732f3c47a3a76f8c0a6cc330e47be57cf0
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2024-03-05 23:43:54 -0600

    establish new balance, start coding click event handler

--- src/pages/NewTransactionPage/NewTransactionPage.jsx
+++ src/pages/NewTransactionPage/NewTransactionPage.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 export default function NewTransactionPage({ balance, setBalance }) {
-    
+    const [newBalance, setBewBalance] = useState(0)
+
+    function handleClick(evt) {
+        evt.preventDefault()
+        console.log(evt.target.value)
+        setNewBalance(evt.target.value)
+    }
     return (<h1>NewTransactionPage</h1>)
 }
