84ccafb8cb0c4bfdc3d9d8a7b1751d601461cba6
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2024-03-20 17:36:25 -0500

    pin footer to bottom of window, needs background added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
+import '../Footer/Footer.css'
+
 export default function Footer() {
 
     
     return (
-        <div className="footer">
+        <div className="Footer">
             <h4>© 2024 <a href="https://www.tifaniethomas.com/" target="_blank">Tifanie Thomas</a>. All rights reserved.</h4>
         </div>
     )
